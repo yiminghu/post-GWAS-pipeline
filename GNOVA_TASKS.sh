cc5acde9b24c75239ddf1dca5b959e96a3c1822d
Yiming Hu <yiminghu91@gmail.com>
2018-05-01 19:43:37 -0400

    update pipelines

---
+++ GNOVA_TASKS.sh
@@ -0,0 +1,28 @@
+'''
+GNOVA 
+SOFTWARE SETTINGS
+'''
+GNOVA_PATH=/ysm-gpfs/pi/zhao/from_louise/yh367/GNOVA/GNOVA-master #4
+REF_TABLE_PATH=/gpfs/ysm/pi/zhao/from_louise/yh367/VA/workflow/GNOVA/sumstats/match_table_full.csv #6
+## reference table, header: Categ (source of published GWAS), 
+## Trait (trait name), Curated_file (path to munged sumstats), Code (index for disease order)
+## e.g. /gpfs/ysm/pi/zhao/from_louise/yh367/VA/workflow/GNOVA/sumstats/match_table_full.csv
+## note this table requires updating if position of any munged sumstats changes. 
+REF_GENOME_PATH=/ysm-gpfs/pi/zhao/from_louise/yh367/GNOVA/genotype_1KG_eur_SNPmaf5/  #7
+## 1000 Genomes genotype data for estimating LD matrix
+##e.g. /ysm-gpfs/pi/zhao/from_louise/yh367/GNOVA/genotype_1KG_eur_SNPmaf5/
+
+'''
+INPUT & OUTPUT SETTINGS
+'''
+
+output_path="${temp}/gnova_ns/" #1 a folder to save all GNOVA output files (there will be ~2,500 files generated)
+mkdir output_path
+output_prefix=${TRAIT_NAME} #2 prefix of output files
+sumstats=${MUNGED_PATH} #3 path to input munged summary stats
+task_file="${TASK_FOLDER}/gnova_ns.task" #5 output path for simple queue task list
+
+'''
+TASK FILES SETTINGS (for parallell running purpose)
+'''
+Rscript --vanilla gc_gnova_cmd.R ${output_path} ${output_prefix} ${sumstats} ${GNOVA_PATH} ${task_file} ${REF_TABLE_PATH} ${REF_GENOME_PATH}
--- gc_gnova_cmd.R
+++ gc_gnova_cmd.R
@@ -3,15 +3,20 @@ options(stringsAsFactors=F)
 
 
 args = commandArgs(trailingOnly=TRUE)
-aname = args[1] ## trait name
-out_path = args[2] ## path to sumstats
-sumstats = args[3] ## path to munged summary stats
+output_path = args[1] ## a folder to save all GNOVA output files (there will be ~2,500 files generated)
+output_prefix = args[2] ## prefix of output files
+sumstats = args[3] ## path to input munged summary stats
 gnova_path = args[4] ## gnova path: /ysm-gpfs/pi/zhao/from_louise/yh367/GNOVA/GNOVA-master
 sqpath = args[5] ## output path for simple queue task list
-ref_path = args[6] ##/gpfs/ysm/pi/zhao/from_louise/yh367/VA/workflow/GNOVA/sumstats/match_table_full.csv
-bfile_path = args[7] ##/ysm-gpfs/pi/zhao/from_louise/yh367/GNOVA/genotype_1KG_eur_SNPmaf5/
+ref_path = args[6] ## reference table, header: Categ (source of published GWAS), 
+## Trait (trait name), Curated_file (path to munged sumstats), Code (index for disease order)
+## e.g. /gpfs/ysm/pi/zhao/from_louise/yh367/VA/workflow/GNOVA/sumstats/match_table_full.csv
+## note this table requires updating if position of any munged sumstats changes. 
+bfile_path = args[7] ## 1000 Genomes genotype data for estimating LD matrix
+##e.g. /ysm-gpfs/pi/zhao/from_louise/yh367/GNOVA/genotype_1KG_eur_SNPmaf5/
 
-mat_table_full = read.csv(ref_path, header = T)
-dir.create(paste0(out_path, aname))
-cmds = paste0('module load Python; python ', gnova_path, 'gnova.py ',sumstats,' ',mat_table_full$Curated_file,' --bfile ', bfile_path, 'eur_chr@_SNPmaf5 --out ',out_path, aname,'/',aname,'_',mat_table_full$Code,'_NS.txt;')    
-writeLines(cmds, paste0(sqpath, aname, '_gnova_ns'))
+mat_table_full = read.csv(ref_path, header = T) ## read 
+dir.create(output_path, showWarnings = FALSE) ## mkdir if not exist
+cmds = paste0('module load Python; python ', gnova_path, 'gnova.py ',sumstats,' ',mat_table_full$Curated_file,' --bfile ', bfile_path, 'eur_chr@_SNPmaf5 --out ',out_path,'/',output_prefix,'_',mat_table_full$Code,'_NS.txt;')    
+writeLines(cmds, paste0(sqpath))
+print(paste0('GNOVA task file written to ', sqpath))
