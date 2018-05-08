5ff67501ef2816d6c310c2a2074260b9bffa02f9
Yiming Hu <yiminghu91@gmail.com>
2018-05-08 17:58:18 -0400

    add

--- pipeline.sh
+++ pipeline.sh
@@ -12,9 +12,21 @@ Input GWAS parameters
 '''
 TRAIT_NAME=example
 sumstats=/gpfs/ysm/pi/zhao/from_louise/bl537/GWAS/VA/Pipeline/Grand/example/SumStats.txt
-grandfolder=/gpfs/ysm/pi/zhao/from_louise/bl537/GWAS/VA/Pipeline/Grand ##path for saving 
+grandfolder=${PIPELINE_PATH}/post-GWAS-pipeline/grandfolder/ ##path for saving 
 
-## No need to change ##
-cd /gpfs/ysm/pi/zhao/from_louise/bl537/GWAS/VA/Pipeline/Script
-python2.7 Pipeline.py --study ${study} --sumstats ${sumstats} --grandfolder ${grandfolder}
+## Generate Standard GWAS / munge / LDSC task files ##
+cd ${PIPELINE_PATH}/post-GWAS-pipeline/
+python2.7 standardGWAS_munge_ldsc.py --study ${TRAIT_NAME} --sumstats ${sumstats} --grandfolder ${grandfolder} --ldsc_path ${LDSC_PATH} --locuszoom_path ${LOCUSZOOM_PATH}
+## this step will generate the following files: 
 
+## Generate GNOVA task files ##
+REF_GENOME_PATH=${GNOVA_PATH}/genotype_1KG_eur_SNPmaf5/
+GNOVA_results=${grandfolder}/GNOVA_results/
+mkdir ${GNOVA_results}
+gnova_output_path=${GNOVA_results}/${TRAIT_NAME}/ #1 a folder to save all GNOVA output files (there will be ~2,500 files generated)
+mkdir ${gnova_output_path}
+gnova_output_prefix=${TRAIT_NAME} #2 prefix of output files
+sumstats=${MUNGED_PATH} #3 path to input munged summary stats
+task_file="${TASK_FOLDER}/gnova_ns.task" #5 output path for simple queue task list
+
+Rscript --vanilla gc_gnova_cmd.R ${grandfolder} ${gnova_output_prefix} ${sumstats} ${GNOVA_PATH} ${task_file} ${REF_TABLE_PATH} ${REF_GENOME_PATH}
